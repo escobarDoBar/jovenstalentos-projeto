5a9eb5c2c75ec0a5d53631aee493e8c8bc029913
escobarDobar <ju_pn2010@protonmail.com>
2023-04-02 09:50:59 -0300

    Reconfigura para unificar os aeroportos.

--- normalizacao/normalizacaoDF.py
+++ normalizacao/normalizacaoDF.py
@@ -6,8 +6,7 @@ pth_db: str = 'db'
 
 pth_csv: str = f'{pth_db}/anac.csv'
 
-pth_pqe_org: str = f'{pth_db}/parquet.origem'
-pth_pqe_dest: str = f'{pth_db}/parquet.destino'
+pth_pqe_aer: str = f'{pth_db}/parquet.aeroporto'
 pth_pqe_empresa: str = f'{pth_db}/parquet.empresa'
 pth_pqe_voo: str = f'{pth_db}/parquet.voo'
 
@@ -22,10 +21,9 @@ def getDadosVoo(voo_id: int):
 
 # Separação dos aeroportos de origem.
 print('[cyan][*][/cyan] Extraindo Aeroporto de Origem...')
-org_df: pd.DataFrame = m_df[
+aer_org_df: pd.DataFrame = m_df[
 	[ 'aeroporto_de_origem_sigla', 'aeroporto_de_origem_nome', 'aeroporto_de_origem_uf', 'aeroporto_de_origem_regiao', 'aeroporto_de_origem_pais', 'aeroporto_de_origem_continente' ]
-].drop_duplicates().reset_index().rename( columns = {
-		'index' : 'id_origem',
+].drop_duplicates().rename( columns = {
 		'aeroporto_de_origem_sigla' : 'sigla',
 		'aeroporto_de_origem_nome' : 'nome', 
 		'aeroporto_de_origem_uf' : 'uf', 
@@ -36,22 +34,11 @@ org_df: pd.DataFrame = m_df[
 )
 ''' Aeorporto de Origem. '''
 
-def getIdOrigemVoo(voo_id: int):
-	''' Retorna o ID (dest_df.id_destino) do aeroport. '''
-	# Extrai os dados de voo.
-	dados_voo = getDadosVoo(voo_id)
-	# Extrai o ID do aeroport.
-	id_origem: int = org_df[ org_df.sigla.isin(dados_voo.aeroporto_de_origem_sigla) & org_df.nome.isin(dados_voo.aeroporto_de_origem_nome) ].id_origem.to_list()[0]
-	# Retorno.
-	return id_origem
-
-
 # Separação dos aeroportos de destino.
 print('[cyan][*][/cyan] Extraindo Aeroporto de Destino...')
-dest_df: pd.DataFrame = m_df[
+aer_dest_df: pd.DataFrame = m_df[
 	[ 'aeroporto_de_destino_sigla', 'aeroporto_de_destino_nome', 'aeroporto_de_destino_uf', 'aeroporto_de_destino_regiao', 'aeroporto_de_destino_pais', 'aeroporto_de_destino_continente' ]
-].drop_duplicates().reset_index().rename( columns={
-		'index' : 'id_destino',
+].drop_duplicates().rename( columns={
 		'aeroporto_de_destino_sigla' : 'sigla',
 		'aeroporto_de_destino_nome' : 'nome', 
 		'aeroporto_de_destino_uf' : 'uf', 
@@ -76,16 +63,29 @@ dest_df: pd.DataFrame = m_df[
 # print(f'Com: {com_dpl}')
 # print(sem_dpl == com_dpl)
 
-def getIdDestinoVoo(voo_id: int):
-	''' Retorna o ID (dest_df.id_destino) do aeroport. '''
+# Merge dos Aeroportos.
+print('[cyan][*][/cyan] Unificando aeroportos de origem e destino...')
+aer_df: pd.DataFrame = pd.concat([aer_org_df, aer_dest_df]).drop_duplicates().reset_index().rename(columns={'index' : 'id_aeroporto'})
+
+def getIdAeroporto(voo_id: int, origem: bool = True):
+	''' Retorna o ID (aer_df.id_aeroporto) do aeroporto. 
+	@params
+	voo_id: int
+	ID do voo.
+	origem: bool = True
+	Informa se deseja extrair os dados de voo do aeroporto de origem, ou destino.
+	'''
 	# Extrai os dados de voo.
 	dados_voo = getDadosVoo(voo_id)
-	# Extrai o ID do aeroport.
-	id_destino: int = dest_df[ dest_df.sigla.isin(dados_voo.aeroporto_de_destino_sigla) & dest_df.nome.isin(dados_voo.aeroporto_de_destino_nome) ].id_destino.to_list()[0]
+	# Extrai o ID do aeroporto.
+	if origem:
+		id_aeroporto: int = aer_df[ aer_df.sigla.isin(dados_voo.aeroporto_de_origem_sigla) & aer_df.nome.isin(dados_voo.aeroporto_de_origem_nome) ].id_aeroporto.to_list()[0]
+	else:
+		id_aeroporto: int = aer_df[ aer_df.sigla.isin(dados_voo.aeroporto_de_destino_sigla) & aer_df.nome.isin(dados_voo.aeroporto_de_destino_nome) ].id_aeroporto.to_list()[0]
 	# Retorno.
-	return id_destino
+	return id_aeroporto
 
-
+print('[cyan][*][/cyan] Extraindo empresas...')
 empresa_df: pd.DataFrame = m_df[
 	[ 'empresa_sigla', 'empresa_nome', 'empresa_nacionalidade' ]
 ].drop_duplicates().reset_index().rename( columns={
@@ -111,6 +111,7 @@ def getIdEmpresaVoo(voo_id: int):
 	# Retorno.
 	return id_empresa
 
+print('[cyan][*][/cyan] Extraindo voos...')
 voos_df: pd.DataFrame = m_df[
 	[
 		'ano',
@@ -135,18 +136,16 @@ voos_df: pd.DataFrame = m_df[
 # print(m_df[m_df.index == 1][ [ 'empresa_sigla', 'empresa_nome' ] ])
 # print(empresa_df[empresa_df.index.isin(getIdEmpresaVoo(1))][ ['sigla', 'nome'] ] )
 
-# TODO: Tenho que unificar as tabelas aeroporto_origem com aeroporto_destino.
-
 ## Relacionamentos.
 # Empresa.
 print('[cyan][*][/cyan] Relacionando [green]Voo[/green] [blue]->[/blue] [green]Empresa[/green]...')
 voos_df['id_empresa'] = voos_df.id_voo.apply(lambda id: getIdEmpresaVoo(id))
 # Aeroporto Origem.
 print('[cyan][*][/cyan] Relacionando [green]Voo[/green] [blue]->[/blue] [green]Aeroporto Origem[/green]...')
-voos_df['id_origem'] = voos_df.id_voo.apply(lambda id: getIdOrigemVoo(id))
+voos_df['id_origem'] = voos_df.id_voo.apply(lambda id: getIdAeroporto(id))
 # Aeroport Destino.
 print('[cyan][*][/cyan] Relacionando [green]Voo[/green] [blue]->[/blue] [green]Aeroporto Destino[/green]...')
-voos_df['id_destino'] = voos_df.id_voo.apply(lambda id: getIdDestinoVoo(id))
+voos_df['id_destino'] = voos_df.id_voo.apply(lambda id: getIdAeroporto(id, origem=False))
 
 ## Exportação.
 # Voos.
@@ -155,9 +154,6 @@ voos_df.to_parquet(pth_pqe_voo)
 # Empresas.
 print(f'[cyan][*][/cyan] Convertendo [green]empresa[blue]_DF[/blue][/green] para [yellow]{pth_pqe_empresa}[/yellow]')
 voos_df.to_parquet(pth_pqe_empresa)
-# Aeroportos Origem.
-print(f'[cyan][*][/cyan] Convertendo [green]origem[blue]_DF[/blue][/green] para [yellow]{pth_pqe_org}[/yellow]')
-voos_df.to_parquet(pth_pqe_org)
-# Aeroportos Destino.
-print(f'[cyan][*][/cyan] Convertendo [green]destino[blue]_DF[/blue][/green] para [yellow]{pth_pqe_dest}[/yellow]')
-voos_df.to_parquet(pth_pqe_dest)
+# Aeroportos.
+print(f'[cyan][*][/cyan] Convertendo [green]aeroporto[blue]_DF[/blue][/green] para [yellow]{pth_pqe_aer}[/yellow]')
+voos_df.to_parquet(pth_pqe_aer)
