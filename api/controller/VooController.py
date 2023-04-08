44d8a594341a51b7d0c8e4bbead317872d4ababb
escobarDobar <ju_pn2010@protonmail.com>
2023-04-07 23:20:02 -0300

    Cria uma função para o Join. Cria a rota para extrair tipos de voo.

--- api/controller/VooController.py
+++ api/controller/VooController.py
@@ -19,7 +19,43 @@ class VooController(DataFrameController):
 		@api.get('/voo/get')
 		def chamarGetVoos(qnt_exibir: int = 0):
 			return self.getVoos(api, qnt_exibir)
+		@api.get('/voo/tipos')
+		def chamarGetTipoVoo(qnt_exibir: int = 0):
+			return self.getTipoVoo(api, qnt_exibir)
 
+	def __getMerged(self, api):
+		''' Retorna um DataFrame dos voos com Joins aplicados. 
+		@params
+		api: FastAPI
+		Carrega o objeto da API, utilizo para acessar outros DataFrames.
+		'''
+		# Join com Origem.
+		join_df: pd.DataFrame = self.df.merge(right = api.ctrl_aer.df, how = 'inner', left_on='id_origem', right_on='id_aeroporto').rename(columns={
+			'sigla' : 'origem_sigla',
+			'nome' : 'origem_nome',
+			'uf' : 'origem_uf',
+			'regiao' : 'origem_regiao',
+			'pais' : 'origem_pais',
+			'continente' : 'origem_continete'
+		})
+		# Join com Destino.
+		join_df = join_df.merge(right = api.ctrl_aer.df, how = 'inner', left_on='id_destino', right_on='id_aeroporto').rename(columns={
+			'sigla' : 'destino_sigla',
+			'nome' : 'destino_nome',
+			'uf' : 'destino_uf',
+			'regiao' : 'destino_regiao',
+			'pais' : 'destino_pais',
+			'continente' : 'destino_continete'
+		})
+		# Join com Empresa.
+		join_df = join_df.merge(right = api.ctrl_emp.df, how = 'inner', left_on='id_empresa', right_on='id_empresa').rename(columns={
+			'sigla' : 'empresa_sigla',
+			'nome' : 'empresa_nome',
+			'nacionalidade' : 'empresa_nacionalidade'
+		})
+
+		# Retorno.
+		return join_df
 	
 	def abrir(self) -> int:
 		''' Abre o DataFrame. '''
@@ -55,31 +91,38 @@ class VooController(DataFrameController):
 		0: Desativa o limite;
 		>0: Informa o limite.
 		'''
-		# Join com Origem.
-		tmp_df: pd.DataFrame = self.df.merge(right = api.ctrl_aer.df, how = 'inner', left_on='id_origem', right_on='id_aeroporto').rename(columns={
-			'sigla' : 'origem_sigla',
-			'nome' : 'origem_nome',
-			'uf' : 'origem_uf',
-			'regiao' : 'origem_regiao',
-			'pais' : 'origem_pais',
-			'continente' : 'origem_continete'
-		})
-		# Join com Destino.
-		tmp_df = tmp_df.merge(right = api.ctrl_aer.df, how = 'inner', left_on='id_destino', right_on='id_aeroporto').rename(columns={
-			'sigla' : 'destino_sigla',
-			'nome' : 'destino_nome',
-			'uf' : 'destino_uf',
-			'regiao' : 'destino_regiao',
-			'pais' : 'destino_pais',
-			'continente' : 'destino_continete'
-		})
-		# Join com Empresa.
-		tmp_df = tmp_df.merge(right = api.ctrl_emp.df, how = 'inner', left_on='id_empresa', right_on='id_empresa')
+		# Define o JOIN.
+		join_df: pd.DataFrame = self.__getMerged(api)
 
 		# DEBUG.
-		print(tmp_df.head(10))
+		print(join_df.head(10))
 
 		# Retorno.
 		if qnt_exibir:
-			return self.head(tmp_df, qnt_exibir)
-		return tmp_df.to_json(orient='records')
+			return self.head(join_df, qnt_exibir)
+		return join_df.to_json(orient='records')
+	
+	def getTipoVoo(self, api, qnt_exibir: int = 0):
+		''' Retorna a origem, destino e empresa do voo, junto com o seu tipo.
+		@params
+		api: FastAPI
+		Carrega o objeto da API, utilizo para acessar outros DataFrames.
+		qnt_exibir: int = None
+		Quantos voos serão exibidos.
+		0: Desativa o limite;
+		>0: Informa o limite.
+		'''
+		# Define o JOIN.
+		join_df: pd.DataFrame = self.__getMerged(api)
+
+		# Salva só as colunas desejadas.
+		join_df = join_df[ [ 'id_voo', 'ano', 'mes', 'natureza', 'grupo_de_voo', 'origem_sigla', 'destino_sigla', 'empresa_sigla' ]]
+
+		#DEBUG.
+		print(join_df.head(10))
+
+		# Retorno.
+		if qnt_exibir:
+			return self.head(join_df, qnt_exibir)
+		return join_df.to_json(orient='records')
+		
